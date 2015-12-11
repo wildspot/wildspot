f12a191f5705e992d2ef735e16af452d14547b7e
Bart Langelaan <langelaanbart@gmail.com>
2015-12-11 05:47:14 +0100

    Add router info and move pages to own files

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,26 @@
-Router.route('/', function () {
+Router.route('/login', function () {
     this.render('Login');
+    this.layout("");
+});
+Router.route('/', function () {
+    this.render('Kaart');
+});
+Router.route('/spot', function () {
+    this.render('Spot');
+});
+Router.route('/informatie', function () {
+    this.render('Informatie');
+});
+Router.route('/kaart', function () {
+    this.render('Kaart');
+});
+Router.route('/over', function () {
+    this.render('Over');
+});
+Router.route('/uitloggen', function () {
+    this.render('Uitloggen');
 });
 
 Router.configure({
-    layoutTemplate: 'WildSpot'
+    layoutTemplate: 'Layout'
 });
