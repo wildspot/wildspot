8a96ba2efcf754f34639c346e70a50f4e67cd48a
Bart Langelaan <langelaanbart@gmail.com>
2015-12-11 08:35:54 +0100

    Spots collection

--- src/app/app.js
+++ src/app/app.js
@@ -54,3 +54,21 @@ if (Meteor.isClient) {
         });
     });
 }
+
+Spots = new Mongo.Collection("spots");
+Spots.attachSchema(new SimpleSchema({
+    time: {
+        type: Date
+    },
+    place: {
+        type: Object
+    },
+    "place.lat": {
+        type: Number,
+        decimal: true
+    },
+    "place.lng": {
+        type: Number,
+        decimal: true
+    }
+}));
