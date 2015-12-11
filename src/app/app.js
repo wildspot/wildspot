5f40e2e4d8dca1e0bf954a28c3e6d09291bd5a71
Bart Langelaan <langelaanbart@gmail.com>
2015-12-11 06:41:26 +0100

    Add kaart

--- src/app/app.js
+++ src/app/app.js
@@ -24,3 +24,33 @@ Router.route('/uitloggen', function () {
 Router.configure({
     layoutTemplate: 'Layout'
 });
+
+if (Meteor.isClient) {
+    Meteor.startup(function() {
+        GoogleMaps.load();
+    });
+
+    Template.Kaart.helpers({
+        exampleMapOptions: function() {
+            // Make sure the maps API has loaded
+            if (GoogleMaps.loaded()) {
+                // Map initialization options
+                return {
+                    center: new google.maps.LatLng(51.917319, 4.484818),
+                    zoom: 16
+                };
+            }
+        }
+    });
+
+    Template.Kaart.onCreated(function() {
+        // We can use the `ready` callback to interact with the map API once the map is ready.
+        GoogleMaps.ready('SpotsMap', function(map) {
+            // Add a marker to the map once it's ready
+            var marker = new google.maps.Marker({
+                position: map.options.center,
+                map: map.instance
+            });
+        });
+    });
+}
