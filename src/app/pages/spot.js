ab220d0e42f5e5f32789e8c2842fa647deb79797
Bart Langelaan <langelaanbart@gmail.com>
2016-01-07 15:29:56 +0100

    Add animal selector

--- src/app/pages/spot.js
+++ src/app/pages/spot.js
@@ -1,4 +1,11 @@
 if (Meteor.isClient) {
+    Template.Spot.helpers({
+        animals: function(){
+            console.log(Animals);
+            return Animals;
+        }
+    });
+
     Template.Spot.events({
         'click #add': function(){
             Spots.insert({time: new Date(), place: Geolocation.latLng()})
