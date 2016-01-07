b341cc12ae8b6a998ad7f09055f056ca394ebe85
Bart Langelaan <langelaanbart@gmail.com>
2016-01-07 16:11:35 +0100

    Make spotting work

--- src/app/pages/spot.js
+++ src/app/pages/spot.js
@@ -7,9 +7,25 @@ if (Meteor.isClient) {
     });
 
     Template.Spot.events({
-        'click #add': function(){
-            Spots.insert({time: new Date(), place: Geolocation.latLng()});
-            Router.go('');
+        'submit #spot-automatisch': function(event){
+            event.preventDefault();
+
+            var location = Geolocation.currentLocation();
+            var place = Geolocation.latLng();
+
+            if(!location || !place){
+                alert("Geen locatie bekend..");
+            }
+            else{
+                Spots.insert({
+                    time: new Date(),
+                    place: place,
+                    animal: event.target.animal.value,
+                    user: (typeof Meteor.userId === "function") ? Meteor.userId() : "anonymous"
+                });
+
+                Router.go('/');
+            }
         }
     });
 }
